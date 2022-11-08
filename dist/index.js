127309d6b2e4a15e8ac0f6a28c82f0c62efbd686
FrederickLamptey <102755255+FrederickLamptey@users.noreply.github.com>
2022-11-08 16:53:19 -0500

    update index.js and index.js.map

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,4 @@
+#! /usr/bin/env node
 "use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
@@ -29,7 +30,7 @@ function listDirContents(filepath) {
             const detailedFilesPromises = files.map((file) => __awaiter(this, void 0, void 0, function* () {
                 let fileDetails = yield fs.promises.lstat(path.resolve(filepath, file));
                 const { size, birthtime } = fileDetails;
-                return { filename: file, "size(KB)": size, created_at: birthtime };
+                return { filename: file, 'size(KB)': size, created_at: birthtime };
             }));
             const detailedFiles = yield Promise.all(detailedFilesPromises);
             console.table(detailedFiles);
